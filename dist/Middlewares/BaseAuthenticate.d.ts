4f698fa65b201f9ecbaf4f367ed32c5d1f9bf330
Quynh Nguyen <quynh@bespokify.com>
2019-03-12 16:50:35 +0700

    Remove old dist

--- dist/Middlewares/BaseAuthenticate.d.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { ExpressMiddlewareInterface } from 'routing-controllers';
-import { ResponseTrait } from '../Traits';
-export declare class BaseAuthenticateMiddleware extends ResponseTrait implements ExpressMiddlewareInterface {
-    constructor();
-    use(req: Request, res: Response, next: NextFunction): any;
-}
